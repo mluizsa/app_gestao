d5b3b25dad02424184a9ce91c30c1a2b3ec9cd8a
mluizsa <mluizsa@gmail.com>
2022-04-18 19:51:22 -0300

    18 de abril de 2022

--- app/Fornecedor.php
+++ app/Fornecedor.php
@@ -3,9 +3,15 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Fornecedor extends Model
 {
+    //trait
+    use SoftDeletes;
+
     protected $table = 'fornecedores';
     protected $fillable = ['nome', 'site', 'email', 'uf'];
+
+
 }
